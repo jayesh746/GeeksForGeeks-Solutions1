5ce1851b088be7251db6882f1666020997bb62fd
JAYESH <jayeshberad49@gmail.com>
2026-07-15 20:04:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Missing in Array/missing-in-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int missingNum(vector<int>& arr) {
+        
+        int sum = 0;
+        
+        for(int i =0; i<arr.size(); i++){
+            sum += arr[i];
+        }
+        
+        int long long n = arr.size()+1;
+       int long long missing = n*(n+1)/2-sum;
+        
+        return missing;
+        
+    }
+};
