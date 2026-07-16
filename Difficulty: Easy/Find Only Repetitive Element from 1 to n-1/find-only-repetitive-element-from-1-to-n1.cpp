d309a0fb317597663607f321a1a87e1866267edf
JAYESH <jayeshberad49@gmail.com>
2026-07-16 19:07:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Only Repetitive Element from 1 to n-1/find-only-repetitive-element-from-1-to-n1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+	public:
+	int findDuplicate(vector<int>& arr) {
+		unordered_map<int, int> mp;
+		
+		for (int i = 0; i<arr.size(); i++) {
+			mp[arr[i]]++;
+		}
+		int repeating = -1;
+		
+		for (int i = 1; i <= arr.size(); i++) {
+			if (mp[i] == 2) {
+				repeating = i;
+			}
+		}
+		return {repeating};
+		
+	}
+};
