7b949ea84d7c7b5de767c87c1c7fdc3cc8b48b81
JAYESH <jayeshberad49@gmail.com>
2026-07-17 20:29:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing And Repeating/missing-and-repeating.cpp
+++ Difficulty: Easy/Missing And Repeating/missing-and-repeating.cpp
@@ -1,24 +1,25 @@
 class Solution {
-  public:
-    vector<int> findTwoElement(vector<int>& arr) {
-        
-        unordered_map<int,int> mp;
-        
-        for(int i = 0; i<arr.size(); i++){
-            mp[arr[i]]++;
-        }
-        
-        int repeating = -1;
-        int missing = -1;
-        
-        for(int i =1; i<=arr.size(); i++){
-            if(mp[i] == 2){
-                repeating = i;
-            }
-            if(mp[i] == 0){
-                missing = i;
-            }
-        }
-        return {repeating, missing};
-    }
+	public:
+	vector<int> findTwoElement(vector<int>& arr) {
+		
+		unordered_map<int, int> mp;
+		
+		for (int i = 0; i<arr.size(); i++) {
+			mp[arr[i]]++;
+		}
+		
+		int repeating = -1;
+		int missing = -1;
+		
+		for (int i = 1; i<=arr.size(); i++) {
+			if (mp[i] == 2) {
+				repeating = i;
+			}
+			if (mp[i] == 0) {
+				missing = i;
+			}
+		}
+		return {repeating, missing};
+		
+	}
 };
