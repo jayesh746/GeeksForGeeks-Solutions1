0e97249a1825f2e145c25ddd169c3eb851a79066
JAYESH <jayeshberad49@gmail.com>
2026-07-17 21:04:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sorted Subsequence of Size 3/sorted-subsequence-of-size-3.cpp
+++ Difficulty: Medium/Sorted Subsequence of Size 3/sorted-subsequence-of-size-3.cpp
@@ -1,37 +1,48 @@
 class Solution {
-	public:
-	vector<int> find3Numbers(vector<int> &arr) {
-		
-		int n = arr.size();
-		
-		vector<int> leftmin(n);
-		vector<int> rightmax(n);
-		
-		leftmin[0] = -1;
-		int smallest = arr[0];
-		
-		for (int i = 1; i<n; i++) {
-			leftmin[i] = smallest;
-			if (arr[i]<smallest) {
-				smallest = arr[i];
-			}
-		}
-		
-		rightmax[n - 1] = -1;
-		int largest = arr[n - 1];
-		
-		for (int i = n - 2; i >= 0; i--) {
-			rightmax[i] = largest;
-			if (arr[i]>largest) {
-				largest = arr[i];
-			}
-		}
-		
-		for (int i = 1; i<n; i++) {
-			if (leftmin[i]<arr[i] && arr[i]<rightmax[i]) {
-				return {leftmin[i], arr[i], rightmax[i]};
-			}
-		}
-		return {};
-	}
+public:
+    vector<int> find3Numbers(vector<int> &arr) {
+
+        int n = arr.size();
+
+        vector<int> leftmin(n);
+        vector<int> rightmax(n);
+
+        leftmin[0] = -1;
+        int smallest = 0;
+
+        for (int i = 1; i < n; i++) {
+
+            if (arr[i] <= arr[smallest]) {
+                smallest = i;
+                leftmin[i] = -1;
+            }
+            else {
+                leftmin[i] = smallest;
+            }
+        }
+
+        rightmax[n - 1] = -1;
+        int largest = n - 1;
+
+        for (int i = n - 2; i >= 0; i--) {
+
+            if (arr[i] >= arr[largest]) {
+                largest = i;
+                rightmax[i] = -1;
+            }
+            else {
+                rightmax[i] = largest;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            if (leftmin[i] != -1 && rightmax[i] != -1) {
+
+                return {arr[leftmin[i]], arr[i], arr[rightmax[i]]};
+            }
+        }
+
+        return {};
+    }
 };
