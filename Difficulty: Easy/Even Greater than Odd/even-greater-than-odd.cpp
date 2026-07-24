af8a6524fb0dfea846b0b47dd211f348d02b55a7
JAYESH <jayeshberad49@gmail.com>
2026-07-24 21:06:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Even Greater than Odd/even-greater-than-odd.cpp
+++ Difficulty: Easy/Even Greater than Odd/even-greater-than-odd.cpp
@@ -1,20 +1,20 @@
 class Solution {
 	public:
 	vector<int> rearrangeArray(vector<int>& arr) {
-	
-	for (int i = 1; i<arr.size(); i++) {
-		if (i%2 == 1) {
-			if (arr[i]<arr[i - 1]) {
-				swap(arr[i], arr[i - 1]);
+		// code here
+		for (int i = 1; i<arr.size(); i++) {
+			if (i%2 == 1) {
+				if (arr[i] <= arr[i - 1]) {
+					swap(arr[i], arr[i - 1]);
+				}
+				
+			}
+			else {
+				if (arr[i]>arr[i - 1]) {
+					swap(arr[i], arr[i - 1]);
+				}
 			}
 		}
-		else {
-			if (arr[i]>arr[i-1]) {
-				swap(arr[i ], arr[i-1]);
-			}
-		}
-		
+		return arr;
 	}
-	return arr;
-}
 };
