9307e4c97e20149626230816ca295d36e5dfc6f5
JAYESH <jayeshberad49@gmail.com>
2026-07-27 19:52:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sorted Subsequence of Size 3/sorted-subsequence-of-size-3.cpp
+++ Difficulty: Medium/Sorted Subsequence of Size 3/sorted-subsequence-of-size-3.cpp
@@ -1,37 +1,38 @@
 class Solution {
-	public:
-	vector<int> find3Numbers(vector<int> &arr) {
-		
-		int n = arr.size();
-		
-		vector<int> leftmin(n);
-		vector<int> rightmax(n);
-		
-		leftmin[0] = -1;
-		int smallest = arr[0];
-		
-		for (int i = 1; i<n; i++) {
-			leftmin[i] = smallest;
-			if (arr[i]<smallest) {
-				smallest = arr[i];
-			}
-		}
-		
-		rightmax[n - 1] = -1;
-		int largest = arr[n - 1];
-		
-		for (int i = n - 2; i >= 0; i--) {
-			rightmax[i] = largest;
-			if (arr[i]>largest) {
-				largest = arr[i];
-			}
-		}
-		
-		for (int i = 1; i<n; i++) {
-			if (leftmin[i]<arr[i] && arr[i]<rightmax[i]) {
-				return {leftmin[i], arr[i], rightmax[i]};
-			}
-		}
-		return {};
-	}
+public:
+    vector<int> find3Numbers(vector<int> &arr) {
+        int n = arr.size();
+
+        vector<int> leftSmall(n, -1);
+        vector<int> rightLarge(n, -1);
+
+        // Step 1: Find smallest element on the left
+        int mini = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > mini)
+                leftSmall[i] = mini;
+            else
+                mini = arr[i];
+        }
+
+        // Step 2: Find largest element on the right
+        int maxi = arr[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] < maxi)
+                rightLarge[i] = maxi;
+            else
+                maxi = arr[i];
+        }
+
+        // Step 3: Find a valid subsequence
+        for (int i = 0; i < n; i++) {
+            if (leftSmall[i] != -1 && rightLarge[i] != -1) {
+                return {leftSmall[i], arr[i], rightLarge[i]};
+            }
+        }
+
+        return {};
+    }
 };
