09dbd1c6920992e3dc8c142971248dc200d146b3
JAYESH <jayeshberad49@gmail.com>
2026-08-02 22:48:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Two Sum in Sorted Array/two-sum-in-sorted-array.cpp
+++ Difficulty: Easy/Two Sum in Sorted Array/two-sum-in-sorted-array.cpp
@@ -1,23 +1,25 @@
 class Solution {
-	public:
-	vector<int> twoSum(vector<int>& arr, int target) {
-		// code here
-		int left = 0;
-		int right = arr.size() - 1;
-		
-		while (left<right) {
-			int sum = arr[left]+arr[right];
-			
-			if (sum == target)
-				return {left + 1, right + 1};
-			
-			else if (sum<target)
-				left++;
-			
-			else
-				right--;
-			
-		}
-		return {-1, -1};
-	}
+  public:
+    vector<int> twoSum(vector<int>& arr, int target) {
+        // code here
+        int left = 0;
+        int right = arr.size()-1;
+        
+        while(right>left){
+            int sum = arr[left]+arr[right];
+            
+            if(sum == target){
+                return{left+1, right+1};
+            }
+            else if(sum>target){
+                right--;
+            }
+            else{
+                left++;
+            }
+             
+             
+        }
+        return{ -1,-1};
+    }
 };
