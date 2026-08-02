84397bfc4ee31162e467f0195395ec995bfa7eec
JAYESH <jayeshberad49@gmail.com>
2026-08-02 23:06:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Pair Sum Closest to 0/pair-sum-closest-to-0.cpp
+++ Difficulty: Medium/Pair Sum Closest to 0/pair-sum-closest-to-0.cpp
@@ -1,30 +1,33 @@
 class Solution {
   public:
     int closestToZero(vector<int> &arr) {
-        // code here
+        
         sort(arr.begin(), arr.end());
         
         int left = 0;
         int right = arr.size()-1;
         int ans = arr[0]+arr[1];
-        while(right>left){
+        while(left<right){
             int sum = arr[left]+arr[right];
+            
             
             if(abs(sum)<abs(ans))
                 ans = sum;
             
-            else if(abs(sum) == abs(ans))
-            ans = max(ans, sum);
-            if(sum<0){
-            left++;
-            }
-            else if(sum>0){
-            right--;
-            }
-            else{ 
-            return 0;
-            }
+            else if(abs(sum)==abs(ans))
+                ans = max(ans, sum);
+            
+               
+               if(sum<0)
+               left++;
+            
+              else if(sum>0)
+              right--;
+              
+              else
+               return 0;
         }
         return ans;
+        
     }
 };
