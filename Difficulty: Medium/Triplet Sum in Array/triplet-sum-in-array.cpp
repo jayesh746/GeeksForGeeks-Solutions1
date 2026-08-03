48ff1423e55656f75c542ed193f54f5f0b1310e1
JAYESH <jayeshberad49@gmail.com>
2026-08-03 19:47:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Triplet Sum in Array/triplet-sum-in-array.cpp
+++ Difficulty: Medium/Triplet Sum in Array/triplet-sum-in-array.cpp
@@ -4,26 +4,22 @@ class Solution {
         // Code Here
         sort(arr.begin(), arr.end());
         
-        
         for(int i = 0; i<arr.size()-2; i++){
             int left = i+1;
             int right = arr.size()-1;
-            
             while(left<right){
-                int sum = arr[i]+arr[left]+arr[right];
-                
-                if(sum == target){
-                    return true;
-                }
-                
-                else if(sum<target){
-                    left++;
-                }
-                
-                else{
-                    right--;
-                }
+            int sum = arr[i]+arr[left]+arr[right];
+            
+            if(sum == target){
+                return true;
             }
+            
+            else if(sum<target)
+            left++;
+            
+            else
+            right--;
+        }
         }
         return false;
     }
