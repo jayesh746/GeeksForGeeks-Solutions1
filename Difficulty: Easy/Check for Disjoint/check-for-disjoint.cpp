938bb8d03cbd464b8dd56236636701381378705f
JAYESH <jayeshberad49@gmail.com>
2026-08-08 12:21:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check for Disjoint/check-for-disjoint.cpp
+++ Difficulty: Easy/Check for Disjoint/check-for-disjoint.cpp
@@ -1,17 +1,20 @@
 class Solution {
-  public:
-    bool areDisjoint(vector<int> &a, vector<int> &b) {
-        // code here
-        unordered_map<int,int> fuck;
-        
-        for(int i = 0; i<a.size(); i++){
-            fuck[a[i]]++;
-        }
-        
-        for(int i = 0; i<b.size(); i++){
-            if(fuck[b[i]]>0)
-            return false;
-        }
-        return true;
-    }
+	public:
+	bool areDisjoint(vector<int> &a, vector<int> &b) {
+		// code here
+		unordered_map<int, int> mp;
+		
+		for (int i = 0; i<a.size(); i++) {
+			mp[a[i]]++;
+		}
+		
+		for (int i = 0; i<b.size(); i++) {
+			if (mp[b[i]]>0) {
+				return false;
+			}
+			
+			
+		}
+		return true;
+	}
 };
