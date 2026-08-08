34b976dd85c2bc38288fe9f8c2f82629e62c584d
JAYESH <jayeshberad49@gmail.com>
2026-08-08 12:30:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check Equal Arrays/check-equal-arrays.cpp
+++ Difficulty: Easy/Check Equal Arrays/check-equal-arrays.cpp
@@ -1,22 +1,20 @@
 class Solution {
-	public:
-	bool checkEqual(vector<int>& a, vector<int>& b) {
-		// code here
-		unordered_map<int, int> mp;
-		unordered_map<int, int> bp;
-		
-		for (int i = 0; i<a.size(); i++) {
-			mp[a[i]]++;
-		}
-		for (int i = 0; i<b.size(); i++) {
-			bp[b[i]]++;
-			
-		}
-		if (mp == bp) {
-			return true;
-		}
-		return false;
-	}
-	
-	
+  public:
+    bool checkEqual(vector<int>& a, vector<int>& b) {
+        // code here
+        unordered_map<int, int> mp;
+        unordered_map<int, int> np;
+        
+        for(int i = 0; i<a.size(); i++){
+            mp[a[i]]++;
+        }
+        for(int i = 0; i<b.size(); i++){
+            np[b[i]]++;
+        }
+        
+        if(mp == np){
+            return true;
+        }
+        return false;
+    }
 };
