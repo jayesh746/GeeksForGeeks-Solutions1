79791726ce8e4a1d64df32edd48b9a88d21ed2a7
JAYESH <jayeshberad49@gmail.com>
2026-08-14 00:01:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Generating All Subarrays/generating-all-subarrays.cpp
+++ Difficulty: Easy/Generating All Subarrays/generating-all-subarrays.cpp
@@ -4,16 +4,15 @@ class Solution {
 		
 		vector<vector<int>>ans;
 		
-		for (int start = 0; start<arr.size(); start++) {
-			for (int end = start; end < arr.size(); end++) {
-				vector<int>temp;
-				
-				for (int k = start; k <= end; k++) {
-					temp.push_back(arr[k]);
-				}
-				ans.push_back(temp);
-			}
-			
+		for(int st = 0; st<arr.size(); st++){
+		    for(int end = st; end<arr.size(); end++){
+		        vector<int>temp;
+		        
+		        for(int k = st; k <= end; k++){
+		            temp.push_back(arr[k]);
+		        }
+		        ans.push_back(temp);
+		    }
 		}
 		return ans;
 		
