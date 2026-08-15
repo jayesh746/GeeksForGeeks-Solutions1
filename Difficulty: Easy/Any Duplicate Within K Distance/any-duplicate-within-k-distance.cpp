1d4c93caf6c036d6b6a396620f25bb57485dcf62
JAYESH <jayeshberad49@gmail.com>
2026-08-15 20:13:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Any Duplicate Within K Distance/any-duplicate-within-k-distance.cpp
+++ Difficulty: Easy/Any Duplicate Within K Distance/any-duplicate-within-k-distance.cpp
@@ -1,16 +1,14 @@
 class Solution {
   public:
     bool checkDuplicatesWithinK(vector<int>& arr, int k) {
-        unordered_map<int, int> ap;
+        unordered_map<int, int> mp;
         
         for(int i = 0; i<arr.size(); i++){
-            
-            if(ap.find(arr[i]) != ap.end()){
-                if(i-ap[arr[i]] <= k){
-                    return true;
-                }
+            if(mp.find(arr[i]) != mp.end()){
+                if(i-mp[arr[i]] <= k)
+                return true;
             }
-            ap[arr[i]]=i;
+            mp[arr[i]] = i;
         }
         return false;
     }
