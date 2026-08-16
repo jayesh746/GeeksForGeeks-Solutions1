a3b5c2da778d345fcb45475e92425888e86bc7cf
JAYESH <jayeshberad49@gmail.com>
2026-08-16 23:24:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/1-D prefix sum/1d-prefix-sum.cpp
+++ Difficulty: Easy/1-D prefix sum/1d-prefix-sum.cpp
@@ -2,14 +2,14 @@ class Solution {
   public:
     vector<int> prefSum(vector<int> &arr) {
         // code here
-        vector<int> ans;
+        vector<int> p;
         int sum = 0;
         for(int i = 0; i<arr.size(); i++){
              sum += arr[i];
             
-            ans.push_back(sum);
+            p.push_back(sum);
         }
-        return ans;
+        return p;
         
     }
 };
