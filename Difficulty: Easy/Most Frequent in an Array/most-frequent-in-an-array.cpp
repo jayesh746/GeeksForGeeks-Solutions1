011d6b872f28952f31a70c617c394953cac4a679
JAYESH <jayeshberad49@gmail.com>
2026-08-18 20:54:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Most Frequent in an Array/most-frequent-in-an-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+	public:
+	int mostFreqEle(vector<int>& arr) {
+		// code here
+		unordered_map<int, int> mp;
+		
+		int maxfreq = 0;
+		int ans = arr[0];
+		
+		for (int i = 0; i<arr.size(); i++) {
+			mp[arr[i]]++;
+		}
+		
+		for (int i = 0; i<arr.size(); i++) {
+			if (mp[arr[i]] > maxfreq) {
+				maxfreq = mp[arr[i]];
+				ans = arr[i];
+			}
+			
+			else if (mp[arr[i]] == maxfreq && arr[i] > ans) {
+				ans = arr[i];
+				
+			}
+		}
+		return ans;
+	}
+};
