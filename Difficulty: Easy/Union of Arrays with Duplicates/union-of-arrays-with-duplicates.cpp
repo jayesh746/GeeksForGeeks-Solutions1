eb40fb7ed5afaabd3b345dc1dfc6545045dd4b71
JAYESH <jayeshberad49@gmail.com>
2026-08-18 20:10:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.cpp
+++ Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.cpp
@@ -2,22 +2,22 @@ class Solution {
   public:
     vector<int> findUnion(vector<int>& a, vector<int>& b) {
         // code here
-        unordered_set<int> st;
-        vector<int> ans;
-        
-        for(int i = 0; i<a.size(); i++){
-            if(st.find(a[i]) == st.end()){
-                st.insert(a[i]);
-                ans.push_back(a[i]);
-            }
-        }
-        
-        for(int i = 0; i<b.size(); i++){
-            if(st.find(b[i]) == st.end()){
-                st.insert(b[i]);
-                ans.push_back(b[i]);
-            }
-        }
-        return ans;
+      unordered_set<int> seta;
+      vector<int> ans;
+      
+      for(int i = 0; i<a.size(); i++){
+          if(seta.find(a[i]) == seta.end()){
+              seta.insert(a[i]);
+              ans.push_back(a[i]);
+          }
+      }
+      
+      for(int i = 0; i<b.size(); i++){
+          if(seta.find(b[i]) == seta.end()){
+              seta.insert(b[i]);
+              ans.push_back(b[i]);
+          }
+      }
+      return ans;
     }
 };
