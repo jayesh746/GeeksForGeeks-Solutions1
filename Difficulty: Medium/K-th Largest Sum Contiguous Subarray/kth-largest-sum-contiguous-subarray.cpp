b42968865bf477d3e42e5f03ca3b95310d14104b
JAYESH <jayeshberad49@gmail.com>
2026-08-23 13:05:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th Largest Sum Contiguous Subarray/kth-largest-sum-contiguous-subarray.cpp
+++ Difficulty: Medium/K-th Largest Sum Contiguous Subarray/kth-largest-sum-contiguous-subarray.cpp
@@ -2,17 +2,18 @@ class Solution {
   public:
     int kthLargest(vector<int> &arr, int k) {
         // code here
-        vector<int> sums;
+        vector<int> a;
         
         for(int i = 0; i<arr.size(); i++){
+            
             int sum = 0;
             
             for(int j = i; j<arr.size(); j++){
-                sum+= arr[j];
-                sums.push_back(sum);
+                sum += arr[j];
+                a.push_back(sum);
             }
         }
-        sort(sums.begin(), sums.end(), greater<int>());
-        return sums[k-1];
+        sort(a.begin(), a.end(), greater<int>());
+        return a[k-1];
     }
 };
