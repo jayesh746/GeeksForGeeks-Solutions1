b167c6b07c031b7c8fa1306e494a2440f4df457b
JAYESH <jayeshberad49@gmail.com>
2026-08-28 20:32:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element/majority-element.cpp
+++ Difficulty: Medium/Majority Element/majority-element.cpp
@@ -2,14 +2,14 @@ class Solution {
   public:
     int majorityElement(vector<int>& arr) {
         // code here
-        unordered_map<int, int> ans;
+        unordered_map<int, int> answer;
         
         for(int i = 0; i<arr.size(); i++){
-            ans[arr[i]]++;
+            answer[arr[i]]++;
         }
         
         for(int i = 0; i<arr.size(); i++){
-            if(ans[arr[i]] > arr.size()/2){
+            if(answer[arr[i]] > arr.size()/2){
                 return arr[i];
             }
         }
