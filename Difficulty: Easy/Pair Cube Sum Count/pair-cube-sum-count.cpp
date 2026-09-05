401a4b0f40a9dc3b235b169298ce67a058c1a08d
JAYESH <jayeshberad49@gmail.com>
2026-09-05 12:49:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Pair Cube Sum Count/pair-cube-sum-count.cpp
+++ Difficulty: Easy/Pair Cube Sum Count/pair-cube-sum-count.cpp
@@ -1,17 +1,16 @@
 class Solution {
   public:
     int pairCubeCount(int n) {
-        
+        // code here
         int count = 0;
         
         for(int a = 1; a*a*a <= n; a++){
-            for(int b = 0; b*b*b <= n; b++){
+            for(int b = 0; b*b*b < n; b++){
                 if(a*a*a + b*b*b == n){
                     count++;
                 }
             }
         }
         return count;
-        
     }
 };
