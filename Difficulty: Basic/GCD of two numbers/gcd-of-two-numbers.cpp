1830d393d2d3faf0ed28650d989622de93167453
JAYESH <jayeshberad49@gmail.com>
2026-09-05 12:51:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/GCD of two numbers/gcd-of-two-numbers.cpp
+++ Difficulty: Basic/GCD of two numbers/gcd-of-two-numbers.cpp
@@ -3,9 +3,9 @@ class Solution {
     int gcd(int a, int b) {
         // code here
         while(b!=0){
-            int rem = a%b;
+            int x = a%b;
             a = b;
-            b = rem;
+            b = x;
         }
         return a;
     }
