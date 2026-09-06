3dd31b2f7a07cc901a1681381b026bf754faecc4
JAYESH <jayeshberad49@gmail.com>
2026-09-06 12:34:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Add two fractions/add-two-fractions.cpp
+++ Difficulty: Basic/Add two fractions/add-two-fractions.cpp
@@ -2,16 +2,16 @@ class Solution {
   public:
     vector<int> addFraction(int num1, int den1, int num2, int den2) {
         // code here
-        int num = num1*den2+num2*den1;
+        int num = num1*den2 + num2*den1;
         int den = den1*den2;
         
         int a = num;
         int b = den;
         
         while(b!=0){
-            int rem = a%b;
+            int r = a%b;
             a = b;
-            b = rem;
+            b = r;
         }
         int gcd = a;
         
@@ -19,6 +19,5 @@ class Solution {
         den = den/gcd;
         
         return {num, den};
-        
     }
 };
