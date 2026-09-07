54b041bbe9b36a5c3443ee1e6d241ae307fcac6a
JAYESH <jayeshberad49@gmail.com>
2026-09-07 21:20:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/nCr/n-cr.cpp
@@ -0,0 +1,17 @@
+class Solution {
+	public:
+	int nCr(int n, int r) {
+		
+		if (r>n) {
+			return 0;
+		}
+		
+		r = min(r, n-r);
+		long long ans = 1;
+		
+		for(int i = 1; i<=r; i++){
+		    ans = ans*(n-r+i)/i;
+		}
+		return ans;
+	}
+};
