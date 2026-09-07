b0e9f0e62485fc6f1575f1ae05556dddded9c87a
JAYESH <jayeshberad49@gmail.com>
2026-09-07 20:25:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Print the Kth Digit/print-the-kth-digit.cpp
+++ Difficulty: Basic/Print the Kth Digit/print-the-kth-digit.cpp
@@ -1,17 +1,16 @@
 class Solution {
   public:
     int kthDigit(int a, int b, int k) {
-        // code here
+        // code her
         int long long n = 1;
-        
         for(int i = 0; i<b; i++){
             n *= a;
         }
         
         for(int i = 1; i<k; i++){
             n = n/10;
+            
         }
-        
         return n%10;
     }
 };
