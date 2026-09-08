29c2072f1f7f0a615d6fabf823e10fb15a1929b5
JAYESH <jayeshberad49@gmail.com>
2026-09-08 20:19:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/nPr/n-pr.cpp
+++ Difficulty: Basic/nPr/n-pr.cpp
@@ -2,8 +2,7 @@ class Solution {
   public:
     long long nPr(int n, int r) {
         // code here
-        int long long ans = 1;
-        
+        long long ans = 1;
         for(int i = 0; i<r; i++){
             ans *= n;
             n--;
