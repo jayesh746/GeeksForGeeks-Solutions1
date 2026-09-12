5c7f44b992ddf86fd84308dace65ebb9384c0c00
JAYESH <jayeshberad49@gmail.com>
2026-09-12 20:17:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Leaders/array-leaders.cpp
+++ Difficulty: Easy/Array Leaders/array-leaders.cpp
@@ -2,15 +2,14 @@ class Solution {
   public:
     vector<int> leaders(vector<int>& arr) {
         // code here
-        vector<int>ans;
-        
+        vector<int> ans;
         int maxright = arr[arr.size()-1];
         ans.push_back(maxright);
         
         for(int i = arr.size()-2; i>=0; i--){
-            if(arr[i]>=maxright){
-                maxright = arr[i];
+            if(arr[i]>= maxright){
                 ans.push_back(arr[i]);
+                maxright = arr[i];
             }
         }
         reverse(ans.begin(), ans.end());
